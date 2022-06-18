e9b12d773f1290ce48ff9dab4906b637090bc010
nhan trach <tumochua14012003@gmail.com>
2022-06-18 18:59:16 +0700

    province

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+   <h1>hello world</h1>
+  </div>
+</template>
+
+<script>
+import { mapState, mapGetters } from "vuex";
+export default {
+  name: "App",
+  components: {},
+  computed: {
+    ...mapState({
+      couter: (state) => state.couter,
+    }),
+    ...mapGetters(["getCouter"]),
+  },
+
+  methods: {
+    handeClick() {
+      this.$store.dispatch("increte");
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
